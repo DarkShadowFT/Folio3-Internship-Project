684d93b97f7f979cd6c27df60146a13582fbd96e
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-07-28 20:41:42 +0500

    Changed type of some fields
    Changed type of _id to Number and Days and Slots available to Array

--- server/models/Doctor.js
+++ server/models/Doctor.js
@@ -1,38 +1,28 @@
 const mongoose = require('mongoose');
 
 const Doctor = new mongoose.Schema({
-  Date:{
-    type: Date,
-    default: Date.now
-  },
-  docotr_id:{
+  _id: Number,
+  Person_ID:{
     type:Number,
     required:true,
     unique:true
   },
-  person_id:{
-    type:Number,
-    required:true,
-    unique:true
-  },
-  specializatioin:{
+  Specialization:{
     type:String,
     required:true
   },
-  degree_info:{
+  Degree_info:{
     type:String,
     required:true
   },
-  days_available:{
-    type:String,
+  Days_available:{
+    type:Array,
     required:true
   },
-  slot_available:{
-    type: Number,
+  Slots_available:{
+    type: Array,
     required: true,
   }
-//2
-
-})
+}, { collection: 'Doctor' })
 
 module.exports = mongoose.model('Doctor', Doctor);
