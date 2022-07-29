3c809ada20038f17e06e2b56d8d8b09662a30385
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-07-29 21:08:43 +0500

    Added Fee in Appointment

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,31 +1,34 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
 
 const AppointmentSchema = new mongoose.Schema({
-    _id: Number,
-    Doctor_ID:{
-      type: Number,
-      require: true,
-      unique: true
-    },
-    Patient_ID:{
-      type: Number,
-      required: true,
-      unique: true
-    },
-    Query:{
-      type: String,
-      required: true,
-    },
-    Date:{
-      type: Date,
-      timestamps: true
-    },
-    Status:{
-      type: String,
-      required: true
-    },
+  _id: Number,
+  Doctor_ID: {
+    type: Number,
+    require: true,
+    unique: true,
+  },
+  Patient_ID: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  Query: {
+    type: String,
+    required: true,
+  },
+  Date: {
+    type: Date,
+    timestamps: true,
+    required: true
+  },
+  Fee: {
+    type: Number,
+    required: true
+  },
+  Status: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-
-module.exports = mongoose.model('appointment', AppointmentSchema);
+module.exports = mongoose.model("appointment", AppointmentSchema);
