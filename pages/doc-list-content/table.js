653512908afaa7b58143750b07c6a4d4b9bf9951
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-08-16 22:59:53 +0500

    Added Pagination in Table
    Signed-off-by: DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>

--- pages/doc-list-content/table.js
+++ pages/doc-list-content/table.js
@@ -1,26 +1,96 @@
 import React, {useEffect, useState} from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import Avatar from '@mui/material/Avatar';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
 import Link from 'next/link'
 import axios from "axios";
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
+import {Avatar, Paper, Button, TextField, IconButton, Container} from '@mui/material';
+import {Box, TableFooter, TablePagination, useTheme} from "@mui/material";
+import {Search, FirstPage, KeyboardArrowLeft, KeyboardArrowRight, LastPage} from '@mui/icons-material';
+
 
 function createData(id, name, specialization, fee, day_availability, time_availability, action) {
   return {id, name, specialization, fee, day_availability, time_availability, action};
 }
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPage /> : <FirstPage />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPage /> : <LastPage />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 export default function BasicTable() {
   const [rows, setRows] = useState([])
   const [searchQuery, setSearchQuery] = useState("");
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const populateTable = async (response) => {
     let counter = 0;
@@ -77,30 +147,41 @@ export default function BasicTable() {
 
   return (
     <>
-    <Paper style={{
-        display: "flex",
-        alignSelf: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: 20,
-      }}
+    <Container style={{
+      display: "flex",
+      alignSelf: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      padding: 20,
+    }}
     >
-      <form align='center' onSubmit={handleSubmit}>
-        <TextField
-          id="search-bar"
-          className="text"
-          label="Enter Doctor's specialization"
-          variant="outlined"
-          value={searchQuery}
-          onChange={searchDoctor}
-          size="small"
-          sx={{ width: 250 }}
-        />
-        <IconButton aria-label="search">
-          <SearchIcon style={{ fill: "blue" }} />
-        </IconButton>
-      </form>
-    </Paper>
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: 20
+        }}
+      >
+        <form align='center' onSubmit={handleSubmit}>
+          <TextField
+            id="search-bar"
+            className="text"
+            label="Enter Doctor's specialization..."
+            variant="outlined"
+            value={searchQuery}
+            onChange={searchDoctor}
+            size="small"
+            sx={{ width: 500 }}
+            component={Paper}
+          />
+          <IconButton aria-label="search">
+            <Search style={{ fill: "blue" }} />
+          </IconButton>
+        </form>
+      </div>
+    </Container>
     <TableContainer component={Paper}>
       <Table sx={{minWidth: 650}} stickyHeader aria-label="simple table">
         <TableHead>
@@ -114,7 +195,10 @@ export default function BasicTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
             <TableRow
               key={row.id}
               sx={{'&:last-child td, &:last-child th': {border: 0}}}
@@ -146,7 +230,33 @@ export default function BasicTable() {
               </TableCell>
             </TableRow>
           ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={6}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
     </>
