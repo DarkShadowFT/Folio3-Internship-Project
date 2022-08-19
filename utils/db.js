03a8d91f1c40733fdf4a321cde3b2752deaddef3
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-08-19 13:23:11 +0500

    Manually added service account file props in firebase admin

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const mongoURI =
+const MONGO_URI =
   "mongodb+srv://<admin>:<QmZLc10qCB6Y9FbZ>@cluster0.do9n7tn.mongodb.net/test";
 const connectToMongo = () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
 
-  mongoose.connect(mongoURI, () => {
+  mongoose.connect(MONGO_URI, () => {
     console.log("Connected to Mongo Successfully");
   });
 };
