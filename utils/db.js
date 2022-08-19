158408c62226054a7404a4d17b8823ebc56dfc07
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-08-19 12:17:05 +0500

    Update connection string to Atlas

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb://localhost:27017/MyDataBase";
+  "mongodb+srv://<admin>:<QmZLc10qCB6Y9FbZ>@cluster0.do9n7tn.mongodb.net/test";
 const connectToMongo = () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
 
-  mongoose.connect("mongodb://localhost:27017/local", () => {
+  mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo Successfully");
   });
 };
