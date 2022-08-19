00c061c16d74f72c66bda320a91da879a6ff3d0a
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-08-19 15:36:51 +0500

    Changed connection string

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const MONGO_URI =
-  "mongodb+srv://<admin>:<QmZLc10qCB6Y9FbZ>@cluster0.do9n7tn.mongodb.net/test";
+  "mongodb+srv://<admin>:<QmZLc10qCB6Y9FbZ>@cluster0.do9n7tn.mongodb.net/find-me-a-doctor";
 const connectToMongo = () => {
   if (mongoose.connections[0].readyState) return;
   // Using new database connection
-
+  console.log("Monoose = " + mongoose.connections)
   mongoose.connect(MONGO_URI, () => {
     console.log("Connected to Mongo Successfully");
   });
